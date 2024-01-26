71ca0fcbdc0ce1cd6c484337cb36478ebd235ed7
蔡伟强 <caiweiqiang@vip.qq.com>
2024-01-26 17:50:26 +0800

    loss endpoint_url parameter

--- docker-buildx.sh
+++ docker-buildx.sh
@@ -46,8 +46,8 @@ fi
 docker_build_cmd="$docker_build_cmd -t $PLUGIN_REPO:$docker_image_tag"
 
 if [[ "${PLUGIN_CACHE:-none}" = "s3" ]]; then
-  docker_build_cmd="$docker_build_cmd --cache-to type=s3,region=${PLUGIN_CACHE_S3_REGION:-us-east-1},bucket=${PLUGIN_CACHE_S3_BUCKET},access_key_id=${PLUGIN_CACHE_S3_ACCESS_KEY},secret_access_key=${PLUGIN_CACHE_S3_SECRET_KEY},mode=${PLUGIN_CACHE_MODE:-min},ignore-error=${PLUGIN_CACHE_IGNORE_ERROR:-false},name=$PLUGIN_REPO:$docker_image_tag"
-  docker_build_cmd="$docker_build_cmd --cache-from type=s3,region=${PLUGIN_CACHE_S3_REGION:-us-east-1},bucket=${PLUGIN_CACHE_S3_BUCKET},access_key_id=${PLUGIN_CACHE_S3_ACCESS_KEY},secret_access_key=${PLUGIN_CACHE_S3_SECRET_KEY},name=$PLUGIN_REPO:$docker_image_tag"
+  docker_build_cmd="$docker_build_cmd --cache-to   type=s3,region=${PLUGIN_CACHE_S3_REGION:-us-east-1},bucket=${PLUGIN_CACHE_S3_BUCKET},endpoint_url=${PLUGIN_CACHE_S3_ENDPOINT},access_key_id=${PLUGIN_CACHE_S3_ACCESS_KEY},secret_access_key=${PLUGIN_CACHE_S3_SECRET_KEY},name=${DRONE_REPO},mode=${PLUGIN_CACHE_MODE:-min},ignore-error=${PLUGIN_CACHE_IGNORE_ERROR:-false}"
+  docker_build_cmd="$docker_build_cmd --cache-from type=s3,region=${PLUGIN_CACHE_S3_REGION:-us-east-1},bucket=${PLUGIN_CACHE_S3_BUCKET},endpoint_url=${PLUGIN_CACHE_S3_ENDPOINT},access_key_id=${PLUGIN_CACHE_S3_ACCESS_KEY},secret_access_key=${PLUGIN_CACHE_S3_SECRET_KEY},name=${DRONE_REPO}"
 fi
 
 docker_build_cmd="$docker_build_cmd --push ."
